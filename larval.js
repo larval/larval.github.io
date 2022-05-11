d645d7cbedb9afc06f0ebfa625c04094da0e88ba
larval <admin@larval.com>
2022-05-11 00:37:58 -0400

    Added hotkey support.

--- larval.js
+++ larval.js
@@ -1,4 +1,4 @@
-var L = {
+const L = {
 
 	/* INTERNALS */
 	_stageData: null,
@@ -11,10 +11,46 @@ var L = {
 	_setNextStagePollShort: 30,
 	_marqueeLoopSecondsShort: 60,
 	_marqueeLoopSecondsLong: 120,
+	_marqueeInterval: null,
+	_marqueeFlashTimeout: null,
 	_notifyTitleInterval: null,
 	_notifyAudio: new Audio('larval.mp3'),
 	_notifyAllowed: null,
+	_contentTableRowCountThatAreInView: 10,
+	_emptyCellHtml: '<div class="l_none">&#8226;</div>',
 	_title:	document.title,
+	_keysIgnore: ['ShiftRight','ShiftLeft'],
+	_keyRow: 0,
+	_keyMapIndexDefault: 'Y',
+	_keyMapIndex: null,
+	_keyMap: {
+		A: ['https://www.seekingalpha.com/symbol/@', 'https://www.seekingalpha.com/symbol/@/options', 'https://www.seekingalpha.com/symbol/@-USD'],
+		B: ['https://www.barchart.com/stocks/quotes/@', 'https://www.barchart.com/stocks/quotes/@/options', 'https://www.barchart.com/crypto/coins/@'],
+		C: ['https://www.cnbc.com/quotes/@', 'https://www.cnbc.com/quotes/@?tab=options', 'https://www.cnbc.com/quotes/@.CM='],
+		D: ['https://research.tdameritrade.com/grid/public/research/stocks/summary?symbol=@'],
+		E: ['https://www.etrade.wallst.com/v1/stocks/snapshot/snapshot.asp?symbol=@'],
+		F: ['https://www.finviz.com/quote.ashx?t=@',  null, 'https://www.finviz.com/crypto_charts.ashx?t=@USD'],
+		G: ['https://www.benzinga.com/quote/@', null, 'https://www.benzinga.com/quote/@-USD'],
+		H: ['https://www.stockcharts.com/h-sc/ui?s=@'],
+		I: ['https://www.investing.com/search/?q=@'],
+		J: ['https://www.wsj.com/market-data/quotes/@', 'https://www.wsj.com/market-data/quotes/@/options'],
+		K: ['https://www.morningstar.com/stocks/xnas/@/quote'],
+		L: ['https://www.fool.com/quote/@', null, 'https://www.fool.com/quote/crypto/@'],
+		M: ['https://www.marketwatch.com/investing/stock/@', 'https://www.marketwatch.com/investing/stock/@/options', 'https://www.marketwatch.com/investing/cryptocurrency/@USD'],
+		N: ['https://money.cnn.com/quote/quote.html?symb=@'],
+		O: ['https://www.cboe.com/delayed_quotes/@', 'https://www.cboe.com/delayed_quotes/@/quote_table'],
+		P: ['https://eresearch.fidelity.com/eresearch/goto/evaluate/snapshot.jhtml?symbols=@'],
+		Q: ['https://www.nasdaq.com/market-activity/stocks/@', 'https://www.nasdaq.com/market-activity/stocks/@/option-chain'],
+		R: ['https://www.robinhood.com/stocks/@', null, 'https://www.robinhood.com/crypto/@'],
+		S: ['https://www.stocktwits.com/symbol/@', null, 'https://www.stocktwits.com/symbol/@.X'],
+		T: ['https://www.tradestation.com/research/stocks/@'],
+		U: ['https://www.gurufocus.com/stock/@'],
+		V: ['https://www.tradingview.com/chart/?symbol=@', null, 'https://www.tradingview.com/chart/?symbol=@USD'],
+		W: ['https://www.twitter.com/search?q=%24@', null, 'https://www.twitter.com/search?q=%24@.X'],
+		X: ['https://www.foxbusiness.com/quote?stockTicker=@'],
+		Y: ['https://finance.yahoo.com/quote/@', 'https://finance.yahoo.com/quote/@/options', 'https://finance.yahoo.com/quote/@-USD'],
+		Z: ['https://www.zacks.com/stock/quote/@', 'https://www.zacks.com/stock/quote/@/options-chain']
+	},
 
 	/* SHORTHAND / COMMON */
 	E: id => document.getElementById(id),
@@ -40,15 +76,18 @@ var L = {
 		return(p.textContent || p.innerText || '');
 	},
 	T: tag => document.getElementsByTagName(tag),
+	U: string => { return((string?string[0]:'').toUpperCase() + string.substring(1)); },
 
 	/* EVENTS */
 	onload: () => {
 		L.settingsLoad();
 		L.notifySetup();
+		L.keyMapSetup();
 		document.addEventListener('click', L.notifySetup);
 		document.addEventListener('touchstart', L.notifySetup);
 		document.addEventListener('scroll', L.onscroll);
 		window.addEventListener('resize', L.onresize);
+		window.addEventListener('keydown', L.onkeydown);
 		setTimeout(L.onloadAnimationComplete, 5500);
 		L.E('l_range_up').oninput();
 		L.E('l_range_down').oninput();
@@ -67,10 +106,13 @@ var L = {
 			L.marqueeUpdate(L._marqueeLoopSecondsLong);
 		else
 			L.marqueeInitiate(L._marqueeLoopSecondsShort);
-		setInterval(() => { L.marqueeUpdate(L._marqueeLoopSecondsLong) }, L._marqueeLoopSecondsLong * 1000);
+		L.marqueeIntervalReset();
 		L.updateLiveTable(true);
 	},
-	onresize: () => { L.settingsButtonToggle(true); },
+	onresize: () => {
+		L.settingsButtonToggle(true);
+		L.updateContentTableRowCountThatAreInView();
+	},
 	onscroll: () => {
 		const w=window, d=document, e=d.documentElement, g=L.T('body')[0],
 			x=w.innerWidth||e.clientWidth||g.clientWidth, y=w.innerHeight||e.clientHeight||g.clientHeight,
@@ -80,6 +122,78 @@ var L = {
 		lf.style.top = scrolledDown ? '-28px' : '0';
 		lf.style.maxHeight =  scrolledDown ? '100px' : '';
 		lar.style.height = lal.style.height = scrolledDown ? '72px' : '';
+	},
+	onkeydown: e => {
+		L.fastSplash();
+		if(!L._stageData || (e && (e.ctrlKey || e.altKey || L._keysIgnore.indexOf(e.code) >= 0)))
+			return;
+		const rows=L.E('l_content_table').getElementsByTagName('tr');
+		let lastKeyRow=L._keyRow;
+		if(!L._keyRow) {
+			for(let i=0; i < rows.length; i++) {
+				if(rows[i].matches(':hover')) {
+					lastKeyRow = 0;
+					L._keyRow = i;
+					break;
+				}
+			}
+		}
+		if(e === false)
+			L._keyRow = 0;
+		else if(e) {
+			e.preventDefault();
+			switch(e.code) {
+				case 'Slash':     L.marqueeHotKeyHelp(); break;
+				case 'Escape':    L._keyRow = 0; break;
+				case 'Home':      L._keyRow = 1; break;
+				case 'End':       L._keyRow = rows.length - 1; break;
+				case 'ArrowUp':   L._keyRow--; break;
+				case 'ArrowDown': L._keyRow++; break;
+				case 'PageUp':    L._keyRow-=L._contentTableRowCountThatAreInView; break;
+				case 'PageDown':  L._keyRow+=L._contentTableRowCountThatAreInView; break;
+				case 'Space':
+					let toggleAlertException=L.E('l_content_table').getElementsByTagName('tr')[L._keyRow];
+					if(toggleAlertException && (toggleAlertException=toggleAlertException.querySelector('.l_notify_enable,.l_notify_disable')) && toggleAlertException.onclick)
+						toggleAlertException.onclick();
+					return;
+				case 'Enter': case 'NumpadEnter':
+					if(L._keyRow && rows[L._keyRow])
+						rows[L._keyRow].onclick();
+					return;
+				default:
+					let match=e.code.match(/^(Digit|Numpad)([0-9])$/);
+					if(match)
+						L.setSortStageData(parseInt(match[2]));
+					else if((match=e.code.match(/^Key([A-Z])$/))) {
+						L.setURLFormat(match[1], e.shiftKey);
+						if(L._keyRow && rows[L._keyRow])
+							rows[L._keyRow].onclick();
+					}
+					else
+						L.marqueeFlash(`The &quot;<span class='l_marquee_highlight'>${e.code}</span>&quot; key is not mapped, type &quot;<span class='l_marquee_highlight'>?</span>&quot; to see the supported hotkeys.`);
+					return;
+			}
+		}
+		if(L._keyRow < 0)
+			L._keyRow = 0;
+		else if(L._keyRow >= rows.length)
+			L._keyRow = rows.length - 1;
+		if(!lastKeyRow ^ !L._keyRow) {
+			const addOrRemove = (L._keyRow?'add':'remove');
+			if(lastKeyRow && rows[lastKeyRow])
+				rows[lastKeyRow].classList.remove('l_tr_keyrow_selected');
+			for(let i=1; i < rows.length; i++)
+				rows[i].classList[addOrRemove]('l_tr_keyrow');
+		}
+		if(lastKeyRow && rows[lastKeyRow])
+			rows[lastKeyRow].classList.remove('l_tr_keyrow_selected');
+		rows[L._keyRow].classList.add('l_tr_keyrow_selected');
+		if(L._keyRow > 0)
+			rows[L._keyRow].scrollIntoView({behavior:'smooth', block:'center'});
+	},
+	onmousemoveContentTable: () => {
+		if(L._keyRow)
+			L.onkeydown(false);
 	},
 
 	/* FUNCTIONS */
@@ -149,14 +263,14 @@ var L = {
 		L._setNextStagePollTimeout = null;
 		L.getStageData(true);
 	},
-	forceNextStagePoll: () => { L.setNextStagePollComplete() },
+	forceNextStagePoll: () => { L.setNextStagePollComplete(); },
 	htmlPercent: number => {
 		if(number > 0)
 			return(L.D(Math.abs(number),2) + '%<span class="l_up">&#9650;</span>');
 		else if(number < 0)
 			return(L.D(Math.abs(number),2) + '%<span class="l_down">&#9660;</span>');
 		else
-			return('<div class="l_none">&#8226;</div>');
+			return(L._emptyCellHtml);
 	},
 	settingsButtonToggle: forceClosed => {
 		const controlHeight=(L.E('l_control').scrollHeight > 200 ? L.E('l_control_table').scrollHeight : 250)+'px', grow=(!forceClosed && L.E('l_control').style.height!=controlHeight);
@@ -190,6 +304,16 @@ var L = {
 			}
 		}
 	},
+	keyMapSetup: () => {
+		if(!(L._keyMapIndex=localStorage.getItem('l_keymap_index')))
+			L._keyMapIndex = L._keyMapIndexDefault;
+		for(let key in L._keyMap) {
+			if(!L._keyMap[key][1])
+				L._keyMap[key][1] = L._keyMap[key][0];
+			if(!L._keyMap[key][2])
+				L._keyMap[key][2] = L._keyMap[L._keyMapIndexDefault][2];
+		}
+	},
 	updateRangeDisplay: (id, value) => {
 		if(L.E(id)) L.E(id).innerHTML = (id=='l_range_volume_display' ? value : (value / 10).toFixed(1));
 	},
@@ -217,10 +341,40 @@ var L = {
 		else {
 			for(let i=L._stageData[topType].length-1; i >= 0; i--) {
 				let item = L._stageData[topType][i];
-				html += `<div class="l_marquee_link" onclick="L.openStockWindow('${item[0]}')"><span class='l_marquee_highlight'>#${i+1}</span>${item[0]} &#177; ${item[1]}%</div> `
+				html += `<div class="l_marquee_link" onclick="L.openStockWindow('${item[0]}')"><span class='l_marquee_highlight_padded'>#${i+1}</span>${item[0]} &#177; ${item[1]}%</div> `
 			}
 		}
 		L.marqueeInitiate(seconds, html);
+	},
+	marqueeFlash: message => {
+		if(L.marqueeFlashTimeout)
+			clearTimeout(L.marqueeFlashTimeout);
+		L.E('l_marquee_flash').innerHTML = message ? message : '';
+		L.E('l_marquee').style.display = message ? 'none' : 'inline-block';
+		L.E('l_marquee_flash').style.display = message ? 'inline-block' : 'none';
+		if(message) {
+			window.scrollTo({top: 0, behavior: 'smooth'});
+			L.marqueeIntervalReset();
+			L.marqueeFlashTimeout = setTimeout(L.marqueeFlash, 5000);
+		}
+		else
+			L.marqueeUpdate(L._marqueeLoopSecondsLong);
+	},
+	marqueeIntervalReset: () => {
+		if(L._marqueeInterval)
+			clearInterval(L._marqueeInterval);
+		L._marqueeInterval = setInterval(() => { L.marqueeUpdate(L._marqueeLoopSecondsLong) }, L._marqueeLoopSecondsLong * 1000);
+	},
+	marqueeHotKeyHelp: () => {
+		let key, match, html='<span class="l_marquee_blink">&#8226;</span> The following hotkeys are available to quickly navigate to a variety of third party websites. <span class="l_marquee_blink">&#8226;</span> Use <span class="l_marquee_highlight">&#8645;</span> arrow keys to navigate to a row followed by selecting one of these hotkeys: ';
+		for(let key in L._keyMap) {
+			if((match=L._keyMap[key][0].match(/([a-z]+)\.[a-z]+\//i)))
+				html += `<div class="l_marquee_link" onclick="L.setURLFormat('${key}',false)"><span class='l_marquee_highlight_padded'>${key}</span>${L.H(match[1])}</div> `
+		}
+		html += '<span class="l_marquee_blink">&#8226;</span> Hold down the <span class="l_marquee_highlight">shift</span> key to make your selection permanent. <span class="l_marquee_blink">&#8226;</span> The keys <span class="l_marquee_highlight">1-7</span> can be used to sort by each column.';
+		window.scrollTo({top: 0, behavior: 'smooth'});
+		L.marqueeIntervalReset();
+		L.marqueeInitiate(90, html);
 	},
 	notify: (message) => {
 		L.notifyClear();
@@ -257,7 +411,7 @@ var L = {
 			L._notifyExceptions[symbol] = true;
 		else if(L._notifyExceptions[symbol])
 			delete L._notifyExceptions[symbol];
-		L.updateLiveTable(false);
+		L.updateLiveTable(false, true);
 	},
 	notifyPlayAudio: () => {
 		let playPromise=L._notifyAudio.play();
@@ -284,24 +438,49 @@ var L = {
 		catch (e) { }
 	},
 	openStockWindow: (symbolOrIndex, e) => {
+		let symbol='', urlType=0;
 		if(e && e.target.nodeName != 'TD')
 			return;
-		let symbol = '';
 		if(typeof symbolOrIndex == 'number') {
 			symbol = L._stageData['stocks'][symbolOrIndex][0];
-			if(L._stageData['stocks'][symbolOrIndex][11]) {
+			if(L._stageData['stocks'][symbolOrIndex][11] && e && e.target.className == 'l_company_name') {
 				window.open(L._stageData['stocks'][symbolOrIndex][11], `larval_news_${symbol}`).focus();
 				return;
 			}
+			else if(e && e.target.className == 'l_options')
+				urlType = 1;
 		}
-		else if(typeof symbolOrIndex == 'string')
+		if(typeof symbolOrIndex == 'string')
 			symbol = symbolOrIndex;
-		if(symbol[0] == '*')
-			symbol = symbol.substr(1) + '-USD';
-		window.open(`https://finance.yahoo.com/quote/${L.H(symbol)}`, `larval_${symbol}`).focus();
+		if(symbol[0] == '*') {
+			urlType = 2;
+			symbol = symbol.substr(1);
+		}
+		L.gotoURL(symbol, urlType, `larval_${symbol}`);
+	},
+	createURL: (symbol, urlType) => {
+		let keyMap = L._keyMap[L._keyMapIndex];
+		if(!keyMap)
+			keyMap = L._keyMap[L._keyMapIndexDefault];
+		return(keyMap[urlType].replace('@', symbol));
+	},
+	gotoURL: (symbol, urlType, windowName) => {
+		window.open(L.createURL(symbol, urlType), windowName).focus();
+	},
+	setURLFormat: (key, saveSettings) => {
+		if(!L._keyMap[key])
+			return;
+		L._keyMapIndex = key;
+		const domain=new URL(L._keyMap[L._keyMapIndex][0]), display=(domain && domain.hostname ? domain.hostname : url);
+		if(saveSettings) {
+			localStorage.setItem('l_keymap_index', L._keyMapIndex);
+			L.marqueeFlash(`Links will now permanently direct to <span class='l_marquee_highlight'>${display}</span> by default.`);
+		}
+		else
+			L.marqueeFlash(`Links will now direct to <span class='l_marquee_highlight'>${display}</span> for this session, hold down <span class='l_marquee_highlight'>shift</span> to make it permanent.`);
 	},
 	setSortStageData: column => {
-		if(L._stageDataSortByColumn == -column) {
+		if(L._stageDataSortByColumn == -column || !column || column > L.E('l_content_table').getElementsByTagName('th').length) {
 			if(L._stageData.stocksImmutable)
 				L._stageData.stocks = structuredClone(L._stageData.stocksImmutable);
 			L._stageDataSortByColumn = 0;
@@ -318,9 +497,9 @@ var L = {
 				L._stageData.stocksImmutable = structuredClone(L._stageData.stocks);
 			L._stageData.stocks = L._stageData.stocks.sort((a, b) => {
 				const column = Math.abs(L._stageDataSortByColumn) - 1;
-				if(a[column] === null || a[column] === false)
+				if(a[column] === null || a[column] === false || a[column] === undefined)
 					return 1;
-				else if(b[column] === null || b[column] === false)
+				else if(b[column] === null || b[column] === false || b[column] === undefined)
 					return -1;
 				else if(typeof a[column] == 'string')
 					return L._stageDataSortByColumn < 0 ? b[column].toUpperCase().localeCompare(a[column].toUpperCase()) : a[column].toUpperCase().localeCompare(b[column].toUpperCase());
@@ -331,10 +510,22 @@ var L = {
 		if(updateView)
 			L.updateLiveTable(false);
 	},
-	updateLiveTable: doNotify => {
+	updateContentTableRowCountThatAreInView: () => {
+		let rows=L.E('l_content_table').getElementsByTagName('tr'), total=-5;
+		for(let i=0; i < rows.length; i++) {
+			const box=rows[i].getBoundingClientRect();
+			if(box.top < window.innerHeight && box.bottom >= 0)
+				total++;
+		}
+		if(total < 10)
+			total = 10;
+		L._contentTableRowCountThatAreInView = total;
+		return(total);
+	},
+	updateLiveTable: (doNotify, doNotResetKeyRow) => {
 		if(!L._stageData)
 			return;
-		const columns=['symbol', L._forceContentTableShrink?'<div class="l_none">&#8226;</div>':'company', '~5min%','total%','price','volume','options'];
+		const columns=['symbol', L._forceContentTableShrink?L._emptyCellHtml:'company', '~5min%','total%','price','volume','options'];
 		const rangeUp=parseFloat(L.E('l_range_up_display').innerHTML), rangeDown=parseFloat(L.E('l_range_down_display').innerHTML), rangeVolume=parseInt(L.E('l_range_volume_display').innerHTML)*1000, optionsOnly=L.E('l_options_only').checked, includeCrypto=L.E('l_include_crypto').checked;
 		L.E('l_afterhours_left').style.display = (!L._splashComplete||!L._stageData['afterhours']?'none':'block');
 		L.E('l_afterhours_right').style.display = (!L._splashComplete||!L._stageData['afterhours']?'none':'block');
@@ -362,9 +553,9 @@ var L = {
 				 <div class="l_notify_disable" title="Disable ${L.H(row[0])} notifications for this session" onclick="L.notifyException('${L.H(row[0])}', true)">x</div>
                  ${L.H(row[0])}
                 </td>
-				<td>${L._forceContentTableShrink?'<div class="l_none">&#8226;</div>':L.H(row[1])}</td>
+				<td>${L._forceContentTableShrink?L._emptyCellHtml:L.H(row[1])}</td>
 				<td colspan="4">HALT: ${L.H(row[2])}</td>
-				<td>${row[3]?L.H(row[3]):'<div class="l_none">&#8226;</div>'}</td>
+				<td>${row[3]?L.H(row[3]):L._emptyCellHtml}</td>
 				</tr>`;
 			if(notify) {
 				notifyAny = true;
@@ -405,32 +596,32 @@ var L = {
 			let priceColumn = '<div class="l_hover_container">';
 			if(row[8])
 				priceColumn += `<span class="l_hover_active">${row[8]<0?'-':'+'}$${L.D(Math.abs(row[8]),2)}</span><span class="l_hover_inactive">`;
-			priceColumn += row[4] ? ('$'+L.D(row[4],2)) : '<div class="l_none">&#8226;</div>';
+			priceColumn += row[4] ? ('$'+L.D(row[4],2)) : L._emptyCellHtml;
 			if(row[8])
 				priceColumn += '</span>';
 			priceColumn += '</div>';
 			let volumeColumn = '<div class="l_hover_container">';
 			if(row[9])
 				volumeColumn += `<span class="l_hover_active">+${L.F(row[9],1)}</span><span class="l_hover_inactive">`;
-			volumeColumn += row[5] ? L.F(row[5],1) : '<div class="l_none">&#8226;</div>';
+			volumeColumn += row[5] ? L.F(row[5],1) : L._emptyCellHtml;
 			if(row[9])
 				volumeColumn += '</span>';
 			volumeColumn += '</div>';
 			let companyColumn = '<div class="l_hover_container">';
 			if(row[10] && !L._forceContentTableShrink)
 				companyColumn += `<span class="l_hover_active_left">${L.H(row[10])}</span><span class="l_hover_inactive">`;
-			companyColumn += L._forceContentTableShrink ? '<div class="l_none">&#8226;</div>' : L.H(row[1]);
+			companyColumn += L._forceContentTableShrink ? L._emptyCellHtml : L.H(row[1]);
 			if(row[10] && !L._forceContentTableShrink)
 				companyColumn += '</span>';
 			companyColumn += '</div>';
 			htmlRow = `<tr class="${rowClass}" onclick="L.openStockWindow(${i}, event)">
 				<td>${notifyControl}${L.H(row[0])}</td>
-				<td>${companyColumn}</td>
+				<td class="l_company_name">${companyColumn}</td>
 				<td>${L.htmlPercent(row[2])}</td>
 				<td>${L.htmlPercent(row[3])}</td>
 				<td>${priceColumn}</td>
 				<td>${volumeColumn}</td>
-				<td>${notifyPopout}${row[6]?L.H(row[6]):'<div class="l_none">&#8226;</div>'}</td>
+				<td class="${row[6]?'l_options':''}">${notifyPopout}${row[6]?L.H(row[6]):L._emptyCellHtml}</td>
 				</tr>`;
 			if(notify)
 				htmlPriority += htmlRow;
@@ -444,9 +635,14 @@ var L = {
 		if(!L.E('l_awaiting_data') && !L.E('l_content_table').classList.contains('l_content_tr_fade_in'))
 			L.E('l_content_table').classList.add('l_content_tr_fade_in');
 		L.E('l_content_table').innerHTML = html;
+		L.updateContentTableRowCountThatAreInView();
+		if(!doNotResetKeyRow)
+			L._keyRow = 0;
+		else
+			L.onkeydown(null);
 		if(!L._forceContentTableShrink && L.E('l_content_table').offsetWidth > document.body.offsetWidth) {
 			L._forceContentTableShrink = true;
-			L.updateLiveTable(doNotify);
+			L.updateLiveTable(doNotify, doNotResetKeyRow);
 		}
 		else if(notifyAny && doNotify)
 			L.notify(notifySymbols.length > 0 ? ('Volatile stock(s): '+notifySymbols.join(', ')) : null);
