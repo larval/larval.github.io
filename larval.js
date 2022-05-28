larval/larval.github.io

commit d16ef61dfd12844b010bb5e9495fd845911754a7

Minor cleanup.

larval.js
@@ -700,7 +700,7 @@ const L = {
 						notifyControl = '';
 				}
 				if(['crypto','futures'].indexOf(row[6]) >= 0)
-					rowClass += ` l_${row[6]}`;
+					rowClass = `l_${row[6]}`;
 				let notifyPopout = '';
 				if(row[7] && row[10])
 					notifyPopout = `<div class="l_notify_popout" title="News and earnings on ${L.H(row[7])}">&#128197;&nbsp;${L.H(row[7])}<span>&nbsp;+&nbsp;news</span></div>`;

larval.css
@@ -617,7 +617,7 @@ input[type="checkbox"] {
 	padding-left: 5px;	
 	color: #f00;
 }
-.l_halt, .l_crypto, .l_futures, .l_notify_futures { color: #825900; }
+.l_halt, .l_crypto, .l_futures { color: #825900; }
 .l_notify_popout {
 	position: absolute;
 	top: 2px;

commit 8be97256427c3d6ee89d6dc9051b12ebbf714288

Fixed bug introduced in d16ef61dfd12844b010bb5e9495fd845911754a7 that made futures/crypto notify highlights display incorrectly.

larval.js
@@ -703,7 +703,7 @@ const L = {
 						notifyControl = '';
 				}
 				if(['crypto','futures'].indexOf(row[6]) >= 0)
-					rowClass = `l_${row[6]}`;
+					rowClass += ` l_${row[6]}`;
 				let notifyPopout = '';
 				if(row[7] && row[10])
 					notifyPopout = `<div class="l_notify_popout" title="News and earnings on ${L.H(row[7])}">&#128197;&nbsp;${L.H(row[7])}<span>&nbsp;+&nbsp;news</span></div>`;